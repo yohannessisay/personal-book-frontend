c256f81910875c32357a41f7d78619974b2ec8cf
Sisay <yohannessisay90@gmail.com>
2025-01-09 12:17:38 +0300

    UPD: github ci/cd update

--- src/pages/common/login-modal.vue
+++ src/pages/common/login-modal.vue
@@ -13,7 +13,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@/composables/auth";
-import { Copy, User } from "lucide-vue-next";
+import {  User } from "lucide-vue-next";
 import { ref } from "vue";
 import { toast } from "vue-sonner";
 
